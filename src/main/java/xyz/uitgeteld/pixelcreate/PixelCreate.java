ceb7cf2ef13dbb5dcee9694ce5cda48d02f6bc33
uitgeteld <swendewit07@gmail.com>
2025-10-02 22:34:33 +0200

    Added Sheets
    Added custom sheets; [Aluminum, Platinum, Silver and Wooden]

--- src/main/java/xyz/uitgeteld/pixelcreate/PixelCreate.java
+++ src/main/java/xyz/uitgeteld/pixelcreate/PixelCreate.java
@@ -1,5 +1,6 @@
 package xyz.uitgeteld.pixelcreate;
 
+import net.minecraft.world.flag.FeatureFlagSet;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -7,7 +8,6 @@ import com.mojang.logging.LogUtils;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
@@ -16,7 +16,6 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;
-import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
@@ -46,22 +45,42 @@ public class PixelCreate {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
 
     // Creates a new Block with the id "pixelcreate:example_block", combining the namespace and path
-    public static final DeferredBlock<Block> EXAMPLE_BLOCK = BLOCKS.registerSimpleBlock("example_block", BlockBehaviour.Properties.of().mapColor(MapColor.STONE));
+//    public static final DeferredBlock<Block> EXAMPLE_BLOCK = BLOCKS.registerSimpleBlock("example_block", BlockBehaviour.Properties.of().mapColor(MapColor.STONE));
     // Creates a new BlockItem with the id "pixelcreate:example_block", combining the namespace and path
-    public static final DeferredItem<BlockItem> EXAMPLE_BLOCK_ITEM = ITEMS.registerSimpleBlockItem("example_block", EXAMPLE_BLOCK);
+//    public static final DeferredItem<BlockItem> EXAMPLE_BLOCK_ITEM = ITEMS.registerSimpleBlockItem("example_block", EXAMPLE_BLOCK);
 
-    // Creates a new food item with the id "pixelcreate:example_id", nutrition 1 and saturation 2
-    public static final DeferredItem<Item> EXAMPLE_ITEM = ITEMS.registerSimpleItem("example_item", new Item.Properties().food(new FoodProperties.Builder()
-            .alwaysEdible().nutrition(1).saturationModifier(2f).build()));
+    // Example non-food item
+    public static final DeferredItem<Item> ALUMINUM_SHEET = ITEMS.registerSimpleItem(
+            "aluminum_sheet",
+            new Item.Properties()
+    );
 
+    public static final DeferredItem<Item> PLATINUM_SHEET = ITEMS.registerSimpleItem(
+            "platinum_sheet",
+            new Item.Properties()
+    );
+
+    public static final DeferredItem<Item> SILVER_SHEET = ITEMS.registerSimpleItem(
+            "silver_sheet",
+            new Item.Properties()
+    );
+
+    public static final DeferredItem<Item> WOODEN_SHEET = ITEMS.registerSimpleItem(
+            "wooden_sheet",
+            new Item.Properties()
+    );
     // Creates a creative tab with the id "pixelcreate:example_tab" for the example item, that is placed after the combat tab
-    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
-            .title(Component.translatable("itemGroup.pixelcreate")) //The language key for the title of your CreativeModeTab
-            .withTabsBefore(CreativeModeTabs.COMBAT)
-            .icon(() -> EXAMPLE_ITEM.get().getDefaultInstance())
-            .displayItems((parameters, output) -> {
-                output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
-            }).build());
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB =
+            CREATIVE_MODE_TABS.register("pixelcreate", () -> CreativeModeTab.builder()
+                    .title(Component.translatable("itemGroup.pixelcreate")) // name key
+                    .withTabsBefore(CreativeModeTabs.COMBAT) // where it shows in the menu
+                    .icon(() -> ALUMINUM_SHEET.get().getDefaultInstance()) // tab icon
+                    .displayItems((parameters, output) -> {
+                        output.accept(ALUMINUM_SHEET.get());
+                        output.accept(PLATINUM_SHEET.get());
+                        output.accept(SILVER_SHEET.get());
+                        output.accept(WOODEN_SHEET.get());
+                    }).build());
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
@@ -81,16 +100,11 @@ public class PixelCreate {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
-        // Register the item to a creative tab
-        modEventBus.addListener(this::addCreative);
-
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
     private void commonSetup(FMLCommonSetupEvent event) {
-        // Some common setup code
-        LOGGER.info("HELLO FROM COMMON SETUP");
 
         if (Config.LOG_DIRT_BLOCK.getAsBoolean()) {
             LOGGER.info("DIRT BLOCK >> {}", BuiltInRegistries.BLOCK.getKey(Blocks.DIRT));
@@ -101,17 +115,8 @@ public class PixelCreate {
         Config.ITEM_STRINGS.get().forEach((item) -> LOGGER.info("ITEM >> {}", item));
     }
 
-    // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event) {
-        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
-            event.accept(EXAMPLE_BLOCK_ITEM);
-        }
-    }
-
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
-        // Do something when the server starts
-        LOGGER.info("HELLO from server starting");
     }
 }
