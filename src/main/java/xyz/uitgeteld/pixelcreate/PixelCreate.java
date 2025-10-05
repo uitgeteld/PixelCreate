2b57e2ce345d7d556134eb87b05b66b118d8c010
naive <swendewit07@gmail.com>
2025-10-06 00:26:21 +0200

    removed useless code

--- src/main/java/xyz/uitgeteld/pixelcreate/PixelCreate.java
+++ src/main/java/xyz/uitgeteld/pixelcreate/PixelCreate.java
@@ -1,188 +1,58 @@
 package xyz.uitgeteld.pixelcreate;
 
-import net.minecraft.world.flag.FeatureFlagSet;
-import org.slf4j.Logger;
-
-import com.mojang.logging.LogUtils;
-
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.config.ModConfig;
-import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
-import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
+import org.slf4j.Logger;
+import com.mojang.logging.LogUtils;
 
-// The value here should match an entry in the META-INF/neoforge.mods.toml file
+import xyz.uitgeteld.pixelcreate.item.material.MaterialItems;
+import xyz.uitgeteld.pixelcreate.item.pokeball.NormalPokeballs;
+import xyz.uitgeteld.pixelcreate.item.pokeball.AncientPokeballs;
+
 @Mod(PixelCreate.MODID)
 public class PixelCreate {
-    // Define mod id in a common place for everything to reference
     public static final String MODID = "pixelcreate";
-    // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
-    // Create a Deferred Register to hold Blocks which will all be registered under the "pixelcreate" namespace
-    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
-    // Create a Deferred Register to hold Items which will all be registered under the "pixelcreate" namespace
-    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
-    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "pixelcreate" namespace
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
 
-    // Creates a new Block with the id "pixelcreate:example_block", combining the namespace and path
-//    public static final DeferredBlock<Block> EXAMPLE_BLOCK = BLOCKS.registerSimpleBlock("example_block", BlockBehaviour.Properties.of().mapColor(MapColor.STONE));
-    // Creates a new BlockItem with the id "pixelcreate:example_block", combining the namespace and path
-//    public static final DeferredItem<BlockItem> EXAMPLE_BLOCK_ITEM = ITEMS.registerSimpleBlockItem("example_block", EXAMPLE_BLOCK);
-
-    // Example non-food item
-    public static final DeferredItem<Item> ALUMINUM_SHEET = ITEMS.registerSimpleItem(
-            "aluminum_sheet",
-            new Item.Properties()
-    );
-
-    public static final DeferredItem<Item> PLATINUM_SHEET = ITEMS.registerSimpleItem(
-            "platinum_sheet",
-            new Item.Properties()
-    );
-
-    public static final DeferredItem<Item> SILVER_SHEET = ITEMS.registerSimpleItem(
-            "silver_sheet",
-            new Item.Properties()
-    );
-
-    public static final DeferredItem<Item> WOODEN_SHEET = ITEMS.registerSimpleItem(
-            "wooden_sheet",
-            new Item.Properties()
-    );
-
-    public static final DeferredItem<Item> INCOMPLETE_POKE_BALL = ITEMS.register(
-            "incomplete_poke_ball",
-            () -> new Item(new Item.Properties()) {
-                @Override
-                public boolean isEnabled(FeatureFlagSet enabledFeatures) {
-                    return false;
-                }
-            }
-    );
-
-    public static final DeferredItem<Item> INCOMPLETE_GREAT_BALL = ITEMS.register(
-            "incomplete_great_ball",
-            () -> new Item(new Item.Properties()) {
-                @Override
-                public boolean isEnabled(FeatureFlagSet enabledFeatures) {
-                    return false;
-                }
-            }
-    );
-
-    public static final DeferredItem<Item> INCOMPLETE_ULTRA_BALL = ITEMS.register(
-            "incomplete_ultra_ball",
-            () -> new Item(new Item.Properties()) {
-                @Override
-                public boolean isEnabled(FeatureFlagSet enabledFeatures) {
-                    return false;
-                }
-            }
-    );
-
-    public static final DeferredItem<Item> INCOMPLETE_ANCIENT_POKE_BALL = ITEMS.register(
-            "incomplete_ancient_poke_ball",
-            () -> new Item(new Item.Properties()) {
-                @Override
-                public boolean isEnabled(FeatureFlagSet enabledFeatures) {
-                    return false;
-                }
-            }
-    );
-
-    public static final DeferredItem<Item> INCOMPLETE_ANCIENT_GREAT_BALL = ITEMS.register(
-            "incomplete_ancient_great_ball",
-            () -> new Item(new Item.Properties()) {
-                @Override
-                public boolean isEnabled(FeatureFlagSet enabledFeatures) {
-                    return false;
-                }
-            }
-    );
-
-    public static final DeferredItem<Item> INCOMPLETE_ANCIENT_ULTRA_BALL = ITEMS.register(
-            "incomplete_ancient_ultra_ball",
-            () -> new Item(new Item.Properties()) {
-                @Override
-                public boolean isEnabled(FeatureFlagSet enabledFeatures) {
-                    return false;
-                }
-            }
-    );
-    // Creates a creative tab with the id "pixelcreate:example_tab" for the example item, that is placed after the combat tab
     public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB =
             CREATIVE_MODE_TABS.register("pixelcreate", () -> CreativeModeTab.builder()
                     .title(Component.translatable("itemGroup.pixelcreate")) // name key
                     .withTabsBefore(CreativeModeTabs.COMBAT) // where it shows in the menu
-                    .icon(() -> ALUMINUM_SHEET.get().getDefaultInstance()) // tab icon
+                    .icon(() -> MaterialItems.ALUMINUM_SHEET.get().getDefaultInstance()) // tab icon
                     .displayItems((parameters, output) -> {
-                        output.accept(ALUMINUM_SHEET.get());
-                        output.accept(PLATINUM_SHEET.get());
-                        output.accept(SILVER_SHEET.get());
-                        output.accept(WOODEN_SHEET.get());
-                        output.accept(INCOMPLETE_POKE_BALL.get());
-                        output.accept(INCOMPLETE_GREAT_BALL.get());
-                        output.accept(INCOMPLETE_ULTRA_BALL.get());
-                        output.accept(INCOMPLETE_ANCIENT_POKE_BALL.get());
-                        output.accept(INCOMPLETE_ANCIENT_GREAT_BALL.get());
-                        output.accept(INCOMPLETE_ANCIENT_ULTRA_BALL.get());
+                        // Materials
+                        output.accept(MaterialItems.ALUMINUM_SHEET.get());
+                        output.accept(MaterialItems.PLATINUM_SHEET.get());
+                        output.accept(MaterialItems.SILVER_SHEET.get());
+                        output.accept(MaterialItems.WOODEN_SHEET.get());
+
+                        // Normal Pokéballs
+                        output.accept(NormalPokeballs.INCOMPLETE_POKE_BALL.get());
+                        output.accept(NormalPokeballs.INCOMPLETE_GREAT_BALL.get());
+                        output.accept(NormalPokeballs.INCOMPLETE_ULTRA_BALL.get());
+
+                        // Ancient Pokéballs
+                        output.accept(AncientPokeballs.INCOMPLETE_ANCIENT_POKE_BALL.get());
+                        output.accept(AncientPokeballs.INCOMPLETE_ANCIENT_GREAT_BALL.get());
+                        output.accept(AncientPokeballs.INCOMPLETE_ANCIENT_ULTRA_BALL.get());
                     }).build());
 
-    // The constructor for the mod class is the first code that is run when your mod is loaded.
-    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
-    public PixelCreate(IEventBus modEventBus, ModContainer modContainer) {
-        // Register the commonSetup method for modloading
+    public PixelCreate(IEventBus modEventBus) {
         modEventBus.addListener(this::commonSetup);
 
-        // Register the Deferred Register to the mod event bus so blocks get registered
-        BLOCKS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so items get registered
-        ITEMS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so tabs get registered
+        MaterialItems.ITEMS.register(modEventBus);
+        NormalPokeballs.ITEMS.register(modEventBus);
+        AncientPokeballs.ITEMS.register(modEventBus);
         CREATIVE_MODE_TABS.register(modEventBus);
-
-        // Register ourselves for server and other game events we are interested in.
-        // Note that this is necessary if and only if we want *this* class (PixelCreate) to respond directly to events.
-        // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
-        NeoForge.EVENT_BUS.register(this);
-
-        // Register our mod's ModConfigSpec so that FML can create and load the config file for us
-        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
-    private void commonSetup(FMLCommonSetupEvent event) {
-
-        if (Config.LOG_DIRT_BLOCK.getAsBoolean()) {
-            LOGGER.info("DIRT BLOCK >> {}", BuiltInRegistries.BLOCK.getKey(Blocks.DIRT));
-        }
-
-        LOGGER.info("{}{}", Config.MAGIC_NUMBER_INTRODUCTION.get(), Config.MAGIC_NUMBER.getAsInt());
-
-        Config.ITEM_STRINGS.get().forEach((item) -> LOGGER.info("ITEM >> {}", item));
-    }
-
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-    }
+    private void commonSetup(FMLCommonSetupEvent event) {}
 }
