73ce3eb834cef4bee1dc1c9a671f9c90798e8ceb
uitgeteld <66201378+uitgeteld@users.noreply.github.com>
2025-10-23 23:31:15 +0200

    adding working textures

--- src/main/java/xyz/uitgeteld/pixelcreate/client/ClientModEvents.java
+++ src/main/java/xyz/uitgeteld/pixelcreate/client/ClientModEvents.java
@@ -2,13 +2,13 @@ package xyz.uitgeteld.pixelcreate.client;
 
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.resources.ResourceLocation;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.extensions.common.IClientFluidTypeExtensions;
 import net.neoforged.neoforge.client.extensions.common.RegisterClientExtensionsEvent;
-import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.NotNull;
 import xyz.uitgeteld.pixelcreate.PixelCreate;
 import xyz.uitgeteld.pixelcreate.fluid.ModFluidTypes;
@@ -26,9 +26,8 @@ public class ClientModEvents {
     @SubscribeEvent
     public static void registerClientExtensions(RegisterClientExtensionsEvent event) {
         event.registerFluidType(new IClientFluidTypeExtensions() {
-            private static final ResourceLocation STILL = ResourceLocation.withDefaultNamespace("block/lava_still");
-            private static final ResourceLocation FLOWING = ResourceLocation.withDefaultNamespace("block/lava_flow");
-            private static final ResourceLocation OVERLAY = ResourceLocation.withDefaultNamespace("block/lava_flow");
+            private static final ResourceLocation STILL = ResourceLocation.fromNamespaceAndPath(PixelCreate.MODID, "block/raw_iron_molten_still");
+            private static final ResourceLocation FLOWING = ResourceLocation.fromNamespaceAndPath(PixelCreate.MODID, "block/raw_iron_molten_flow");
 
             @Override
             public @NotNull ResourceLocation getStillTexture() {
@@ -38,16 +37,6 @@ public class ClientModEvents {
             @Override
             public @NotNull ResourceLocation getFlowingTexture() {
                 return FLOWING;
-            }
-
-            @Override
-            public ResourceLocation getOverlayTexture() {
-                return OVERLAY;
-            }
-
-            @Override
-            public int getTintColor() {
-                return 0xFFFF5500;
             }
         }, ModFluidTypes.RAW_IRON_MOLTEN_TYPE.get());
     }
