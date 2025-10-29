685d5f7fe013a0145272feba21c4b43194cb6572
uitgeteld <66201378+uitgeteld@users.noreply.github.com>
2025-10-30 00:08:58 +0100

    Update PixelCreateClient.java

--- src/main/java/xyz/uitgeteld/pixelcreate/PixelCreateClient.java
+++ src/main/java/xyz/uitgeteld/pixelcreate/PixelCreateClient.java
@@ -16,16 +16,10 @@ import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 @EventBusSubscriber(modid = PixelCreate.MODID, value = Dist.CLIENT)
 public class PixelCreateClient {
     public PixelCreateClient(ModContainer container) {
-        // Allows NeoForge to create a config screen for this mod's configs.
-        // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
-        // Do not forget to add translations for your config options to the en_us.json file.
         container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
     }
 
     @SubscribeEvent
     static void onClientSetup(FMLClientSetupEvent event) {
-        // Some client setup code
-//        PixelCreate.LOGGER.info("HELLO FROM CLIENT SETUP");
-//        PixelCreate.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
     }
 }
